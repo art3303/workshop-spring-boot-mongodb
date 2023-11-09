b6237bece48e41f01bc525fe1a60cb91496a1e9f
Arthur de Menezes <arthurmenezes554@gmail.com>
2023-11-09 14:18:34 -0300

    Obtendo um usuário por id

--- src/main/java/com/arthur/Workshopmongo/services/UserService.java
+++ src/main/java/com/arthur/Workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.arthur.Workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.arthur.Workshopmongo.domain.User;
 import com.arthur.Workshopmongo.repository.UserRepository;
+import com.arthur.Workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
 	}
 }
